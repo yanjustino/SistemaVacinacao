c847115d0f6f79191754432190d16cf98830d14a
Yan Justino <yanlimaj@gmail.com>
2014-10-07 22:27:03 -0300

    Commit inicial

---
+++ ControleVacinacao.Dominio/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleVacinacao.Dominio
+{
+   public class Paciente
+   {
+      public int Id { get; set; }
+      public string Nome { get; set; }
+      public string Sexo { get; set; }
+      public DateTime DataNascimento { get; set; }
+      public string TipoSanguineo { get; set; }
+      public string FatorSanguineo { get; set; }
+      public string Email { get; set; }
+      public string Celular { get; set; }
+   }
+}
---
+++ ControleVacinacao.Dominio/Posto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleVacinacao.Dominio
+{
+   public class Posto
+   {
+      public int Id { get; set; }
+      public string Nome { get; set; }
+      public string Tipo { get; set; }
+   }
+}
