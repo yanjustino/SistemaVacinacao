f014e8fae2c8b57488bb6ee2fc76925dc237e70e
Yan Justino <yanlimaj@gmail.com>
2014-10-07 22:46:13 -0300

    Usando Enum para o sexo

--- ControleVacinacao.Dominio/Paciente.cs
+++ ControleVacinacao.Dominio/Paciente.cs
@@ -8,14 +8,15 @@ using System.Threading.Tasks;
 
 namespace ControleVacinacao.Dominio
 {
+   public enum TipoSexo { masculino, feminino};
+
    [Table("Pacientes")]
    public class Paciente
    {
       public int Id { get; set; }
       [StringLength(80)]
       public string Nome { get; set; }
-      [StringLength(1)]
-      public string Sexo { get; set; }
+      public TipoSexo Sexo { get; set; }
       public DateTime DataNascimento { get; set; }
       [StringLength(1)]
       public string TipoSanguineo { get; set; }
